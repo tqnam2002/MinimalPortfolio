bf05521dd187415e42a6bbb8a06a7ebfa98f12ce
tqnaam <quangnam274@gmail.com>
2023-08-10 22:31:48 +0700

    Initial commit

---
+++ minimal-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Intro from './components/Intro';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Timeline from './components/Timeline';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <div>
+      <Intro />
+      <Portfolio />
+      <Timeline />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ minimal-portfolio/src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div>Intro</div>
+  )
+}
+
+export default Intro;
+
+//rfce abbreviation to create jsx boilerplate
