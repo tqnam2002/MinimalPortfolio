d2727d22e7672315590b876d7aabfe68280b990b
tqnaam <quangnam274@gmail.com>
2023-08-14 17:08:23 +0700

    hover timeline added

--- minimal-portfolio/src/App.jsx
+++ minimal-portfolio/src/App.jsx
@@ -66,7 +66,7 @@ function App() {
       <button
         type="button"
         onClick={handleThemeSwitch}
-        className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg p-1 rounded-md"
+        className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg rounded-md"
       >
         {theme === 'dark' ? sun : moon}
       </button>
